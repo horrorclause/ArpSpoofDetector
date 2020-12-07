8db5c6bac3ff20902a7c8d4de6eaf25d4720eddd
Mario J <mjere003@gmail.com>
2020-12-07 12:09:17 -0500

    added arpFile(). Takes completed ARP table data and saves it to a file in the current working directory

--- ArpSpoofDetector.py
+++ ArpSpoofDetector.py
@@ -1,9 +1,9 @@
 """ Checks for duplicate MAC addresses, displays them and outputs to a text file.
     Could be an indicator of a MitM attack."""
 
-import os
-import re
+import os, platform, re
 import itertools
+import datetime
 
 
 broadcastAdd = 'ff-ff-ff-ff-ff-ff'
@@ -13,11 +13,16 @@ duplicates = []
 
 # Gets the ARP table from a Windows Machine
 def getARP():
-    arpData = []
-    with os.popen('arp -a') as f:
-        for i in f.readlines():
-            arpData.append(i)
-    return arpData
+    try:
+        if platform.system() == 'Windows':
+            arpData = []
+            with os.popen('arp -a') as f:
+                for i in f.readlines():
+                    arpData.append(i)
+            return arpData
+    except Exception:
+        print('This is not a windows machine')
+
 #print(getARP())
 
 # Uses Regex to iterate through getARP() data and create a new
@@ -28,7 +33,6 @@ def regIpMac(data):
         ip = re.findall(r"(?:\d{1,3}\.)+(?:\d{1,3})", line)  # Regex for IP address
         mac = re.findall(r"(?:\w{1,2}\-)+(?:\w{1,2})", line)  # Regex for MAC
         if len(ip) and len(mac) > 0:  # Checks to make sure the ip and max regex have contents
-            # TODO: check if they are broadcast addresses and exclude if they are
             # Creates a list of the Ip , Mac in the parent list ipAndMac
             ipAndMac.append([ip[0],mac[0]])
         else:
@@ -36,10 +40,10 @@ def regIpMac(data):
     return ipAndMac
 #print(regIpMac(getARP()))
 
-correctData = regIpMac(getARP())
+correctData = regIpMac(getARP())  #Gets the ARP table and parses it
 
-"""Need to run loop twice to catch all instance of broadcast add. Not sure why.
-    correctData is sanitized of Broadcast Adds."""
+# TODO:Need to run loop twice to catch all instance of broadcast add. Not sure why.
+#      correctData is sanitized of Broadcast Adds.
 for i in correctData:
     for x in i:
         if x == broadcastAdd:
@@ -65,16 +69,34 @@ for i in correctData:
 #print('***----------------***')
 #print(holding)
 
+duplicates.sort()
 
-"""Sorts and dedupes the Duplicates list, and prints out duped machines."""
+"""Dedupes the Duplicates list, and prints out duped machines."""
 
 print('\nThe following machines are being spoofed:')
 print('+'+(len('The following machines are being spoofed: ')*'=') + '+')
-duplicates.sort()
+
 for x in list(duplicates for duplicates,_ in itertools.groupby(duplicates)):  # Sorts duplicates and dedupes
     print('IP: {}'.format(x[0]))
     print('MAC: {}'.format(x[1]))
     print('***'+(len(x[1])*'-')+'***')
 
+# Exports ARP data as file w/Date & Time
+def arpFile():
+    currentTime = datetime.datetime.now().strftime("%m-%d-%Y %H.%M.%S")
+    fileName = 'Duplicated Mac & IP addresses {}.txt'.format(currentTime)
+    file = open(fileName, 'a')
+    file.write('### - {} - ###\n'.format(currentTime))
+    file.write('######'+(len(currentTime)*'#')+'######\n')
 
+    for x in list(duplicates for duplicates, _ in itertools.groupby(duplicates)):  # Sorts duplicates and dedupes
+        file.write('IP: {}\n'.format(x[0]))
+        file.write('MAC: {}\n'.format(x[1]))
+        file.write('***' + (len(x[1]) * '-') + '***\n')
+        file.flush()
+    file.close()
 
+    print('\nThis file saved as: {}'.format(fileName))
+    print('To this directory: {}'.format(os.getcwd()))
+
+arpFile()
